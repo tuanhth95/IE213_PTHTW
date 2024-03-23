22e62654dec9018a0ccd733ee5a660a8a5ba21b2
Pham-Quynh-81 <163506710+Quynh-Pham01@users.noreply.github.com>
2024-03-23 22:30:36 +0700

    Lab1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Bai01 from './Bai1';
+import Bai04 from './Bai4';
+import Bai05 from './Bai5';
+
+function App() {
+  return (
+    <div className="App">
+            <Bai01></Bai01>
+            <Bai04></Bai04>
+            <Bai05></Bai05>
+
+    </div>
+  );
+}
+
+export default App;
